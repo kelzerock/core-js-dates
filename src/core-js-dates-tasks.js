8aa9a44f4033a20d4d5671ce8a6edc45e77742b3
Aleksei Zhuchkov <kelzzzer@gmail.com>
2025-03-02 15:41:59 +0300

    feat: isLeapYear

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -314,8 +314,14 @@ function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
  * Date(2022, 2, 1) => false
  * Date(2020, 2, 1) => true
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const targetYear = date.getFullYear();
+  if (targetYear % 100 === 0) {
+    if (targetYear % 400 === 0) return true;
+    return false;
+  }
+  if (targetYear % 4 === 0) return true;
+  return false;
 }
 
 module.exports = {
